63e34235ccbbf9eeb92a6d21b020b071f47b1b4f
YHYELIM <yhl0513@naver.com>
2025-02-23 14:46:04 +0900

    로딩 추가

---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+    console.log("Loading component triggered");
+    return(
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h1>Loading...</h1>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
 export default function HomePage() {
-  return <h2>🏠 Welcome to the Home Page</h2>;
+  return (
+  <h2>🏠 Welcome to the Home Page</h2>
+);
 }
